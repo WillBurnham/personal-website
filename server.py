fdd175cd7696c5290fa44f5251ba562bc10df38e
WillBurnham <burnham_will@yahoo.com>
2020-07-18 10:56:43 -0500

    Update server.py

--- server.py
+++ server.py
@@ -10,16 +10,29 @@ import xlsxwriter
 import pandas as pd
 import requests
 
+
+#importing the Open Weather Map API key
+owm = pyowm.OWM('ecb7040454382ee36fdd354262f71db1')
+
+
+#SMTP server credentials and target addresses
+send_email = "wb.weather.app@gmail.com"
+rec_email = ["burnham.will2020@gmail.com", "nmgiacomello@gmail.com"]
+password = "2FC018z69420!"
+
+
 #creating cities to be monitored 
 dal = City('Dallas')
 hou = City('Houston')
 aus = City('Austin')
 city_list = [dal, hou, aus]
 
+
 #initializing current weather status
 current_austin_weather = ""
 current_dallas_weather = ""
 current_houston_weather = ""
+
 
 #getting weather status to determine the image we use 
 if aus.find_bad_weather() == True:
@@ -37,39 +50,39 @@ if hou.find_bad_weather() == True:
 else:
     current_houston_weather = "cloud"
 
+
 #routes to the image
 aus_img = "/static/images/" + current_austin_weather + ".png"
 dal_img = "/static/images/" + current_dallas_weather + ".png"
 hou_img = "/static/images/" + current_houston_weather + ".png"
 
-#importing the Open Weather Map API key
-owm = pyowm.OWM('ecb7040454382ee36fdd354262f71db1')
-
-send_email = "wb.weather.app@gmail.com"
-rec_email = ["burnham.will2020@gmail.com", "nmgiacomello@gmail.com"]
-pw = "2FC018z69420!"
 
 #send email when find_bad_weather = true
-def send():   
+def send():
+    
     global current_austin_weather
     global current_dallas_weather
     global current_houston_weather
     global aus_img
     global dal_img
-    global hou_img    
+    global hou_img
+    
     #starting smtp server
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
-    server.login(send_email, pw)   
+    server.login(send_email, password)
+    
     #constructing email body
     message = "It's going to rain tomorrow in "
-    cities = []    
+    cities = []
+    
     #resetting values in each weather column
     current_austin_weather = ""
     current_dallas_weather = ""
     current_houston_weather = ""
     for city in city_list:
-        if city.find_bad_weather() == True:         
+        if city.find_bad_weather() == True:
+            
             #detailing weather at each city so I can apply the appropriate weather icon
             if (city.name == 'Austin'):
                 current_austin_weather = "rain"
@@ -96,6 +109,7 @@ def send():
     message += str(cities)
     message += ". Give Jackson his medicine if you will be either of these places tomorrow."
     
+    
     #reloading image names so they will update dynamically
     aus_img = "/static/images/" + current_austin_weather + ".png"
     dal_img = "/static/images/" + current_dallas_weather + ".png"
@@ -103,6 +117,7 @@ def send():
     
     #sending the email
     server.sendmail(send_email, rec_email, message)
+
 
 #setting up database connection
 def connectToDB():
@@ -114,6 +129,8 @@ def connectToDB():
     )
     return con
 
+
+#scraping page data from website
 def get_content(state, city):
     url = "https://www.trulia.com/" + state + "/" + city
     req = requests.get(url, headers={'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0'})
@@ -121,20 +138,26 @@ def get_content(state, city):
     soup = BeautifulSoup(content, 'html.parser')
     all_property_data = soup.find_all("div", {"class":"Box-sc-8ox7qa-0 jDcCbK"})
     return all_property_data
-
+    
+ 
 #creating excel workbook
 def start_workbook():
     return xlsxwriter.Workbook('real_estate.xlsx')
 
+
 #creating a worksheet from the workbook
-def create_worksheet(workbook):  
+def create_worksheet(workbook):
+    
     worksheet = workbook.add_worksheet()
+    
     worksheet.write('A1', 'Price')
     worksheet.write('B1', 'Beds')
     worksheet.write('C1', 'Bath')
     worksheet.write('D1', 'Address')
     worksheet.write('E1', 'Region')
+    
     return worksheet
+
 
 #grabbing each individual element from the webpage for each property
 def fill_data(all_property_data, worksheet):
@@ -180,76 +203,98 @@ def fill_data(all_property_data, worksheet):
         worksheet.write(''.join(row_d), property_address)
         worksheet.write(''.join(row_e), property_region)
     
+
 #starting background task scheduler to pull weather data behind the scenes
 sched = BackgroundScheduler(daemon=True)
 sched.add_job(send,'cron',minute = '30', hour = '20')
 sched.start()
 
+
 #creating application object
 app = Flask(__name__)
+
 
 #setting up home route
 @app.route('/')
 def home():
     return render_template("index.html")
 
+
 #route to projects page
 @app.route('/projects/')
 def projects():
     return render_template("projects.html", austin_image = aus_img, dallas_image = dal_img, houston_image = hou_img)
+
 
 #route to resume
 @app.route('/resume/') 
 def resume():
     return redirect("/static/Will Burnham Official Engineering Resume 1.pdf") 
 
+
 #route to spreadsheet
 @app.route('/spreadsheet/', methods=['POST'])
-def spreadsheet():   
+def spreadsheet():
+    
     city = request.form['city']
     state = request.form['category']
+    
     workbook = start_workbook()
     worksheet = create_worksheet(workbook)
     content = get_content(state, city)
     fill_data(content, worksheet)
     workbook.close()
+    
     #reading the excel doc with panda's and turning it into an html page for in browser readability
     df = pd.read_excel("real_estate.xlsx")
     xl = df.to_html()
+    
     return xl
+
 
 #route for data submitted through form
 @app.route('/projects/', methods=['POST'])
-def handle_data():       
+def handle_data():    
+    
     #getting word from form
     word = request.form['message']
-    word = word.lower()    
+    word = word.lower()
+    
     #output when no definitions are found
-    no_search_results = "Please enter a valid word."    
+    no_search_results = "Please enter a valid word."
+    
     #connection to db
     con = connectToDB()
-    cursor = con.cursor()    
+    cursor = con.cursor()
+    
     #input validation
     for char in word:
         if (char.isalpha() == False):
-            arr = []
-            arr.append(no_search_results)
-            return render_template("projects.html", my_list = arr, austin_image = aus_img, dallas_image = dal_img, houston_image = hou_img)           
+            return render_template("projects.html", value = no_search_results, austin_image = aus_img, dallas_image = dal_img, houston_image = hou_img)
+            
     #constructing query and fetching results
     query = cursor.execute("SELECT Definition FROM Dictionary WHERE Expression = '%s'" % word)
-    results = cursor.fetchall()    
+    results = cursor.fetchall()
+    
     #if there are results pass up to 3 definitions then close connection
     if results:
         arr = []
         for result in results:
-            arr.append(result[0])        
-        con.close()
-        return render_template("projects.html", my_list = arr, austin_image = aus_img, dallas_image = dal_img, houston_image = hou_img)                  
+            arr.append(result[0])
+        if len(arr) >= 3:
+            con.close()
+            return render_template("projects.html", value = arr[0], value2 = arr[1], value3=arr[2], austin_image = aus_img, dallas_image = dal_img, houston_image = hou_img)
+        if len(arr) == 2:
+            con.close()
+            return render_template("projects.html", value = arr[0], value2 = arr[1], austin_image = aus_img, dallas_image = dal_img, houston_image = hou_img)
+        if len(arr) == 1:
+            con.close()
+            return render_template("projects.html", value = arr[0], austin_image = aus_img, dallas_image = dal_img, houston_image = hou_img)
+           
     else:
-        arr = []
-        arr.append(no_search_results)
         con.close()
-        return render_template("projects.html", my_list = arr, austin_image = aus_img, dallas_image = dal_img, houston_image = hou_img)
+        return render_template("projects.html", value = no_search_results, austin_image = aus_img, dallas_image = dal_img, houston_image = hou_img)
+
           
 if __name__ == "__main__":
     app.run(debug=False)
